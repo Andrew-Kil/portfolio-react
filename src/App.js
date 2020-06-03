d84792a3a9f481bf62f6a6e2dd3a44dec7631969
Andrew KIl <kil.andrew@gmail.com>
2020-06-03 16:07:22 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,71 @@
 import React from "react";
 
-const App = () => {
-  return <div>App</div>;
+import { Switch, Route } from "react-router-dom";
+
+import { useScrollTrigger, Fab, Zoom } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Blog from "./components/Blog";
+
+import "./App.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "fixed",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+const ScrollTop = (props) => {
+  const { children, window } = props;
+  const classes = useStyles();
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      "#back-to-top-anchor"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+};
+
+const App = (props) => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Switch>
+        <Route exact path="/" component={Home}>
+          {Home}
+        </Route>
+        <Route exact path="/" component={Blog}>
+          {Blog}
+        </Route>
+      </Switch>
+      <ScrollTop {...props}>
+        <Fab color="primary" size="small" aria-label="Scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </div>
+  );
 };
 
 export default App;
