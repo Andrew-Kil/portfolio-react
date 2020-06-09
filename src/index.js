e8400665ff554276553775ccc1ea79aced661485
Andrew KIl <kil.andrew@gmail.com>
2020-06-09 17:50:43 -0400

    add ThemeProvider and CssBaseline to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import App from "./App";
+import theme from "./theme";
+import { ThemeProvider } from "@material-ui/core";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
